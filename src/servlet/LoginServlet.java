aa015acab7c7651276dc049d649de40fc54aa7ef
kmsuser <kmsuser@KMS2013-66.kms.local>
2018-05-28 19:40:07 +0900

    競合確認2

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -27,7 +27,9 @@ public class LoginServlet extends HttpServlet {
 		bean.setUserId("");
 		bean.setPassword("");
 
-		for (int i = 0; i < 5; i++) {
+		int i = 0;
+
+		while(i < 9) {
 			System.out.println(i);
 		}
 
